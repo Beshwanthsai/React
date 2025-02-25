535112b53275c8685d0ffb1446955ceaba46fe37
Beshwanthsai <beshwanthsai2005@gmail.com>
2025-02-25 08:25:39 +0530

    counter.js and counter.css this will increment and decrement the value in the webpage

---
+++ React/my_first_app/src/Components/counter.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './counter.css';
+function Counter() {
+   
+    const [count, setCount] = useState(0);
+
+    return (
+        <div className="counter-container">
+            <h1>Counter App</h1>
+            <div className="counter-value">{count}</div>
+            <div className="button-container">
+                <button onClick={() => setCount(count - 1)}>-</button>
+                <br></br>
+                <button onClick={() => setCount(count + 1)}>+</button>
+            </div>
+        </div>
+    );
+}
+
+export default Counter;
