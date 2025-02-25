1d173404b028fc182d0517261ca603aef5968fde
Beshwanthsai <beshwanthsai2005@gmail.com>
2025-02-25 08:05:06 +0530

    created react app

--- React/my_first_app/src/Greetings.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-const Greetings = (props)=>{
-    return <h1>Welcome to React App on 19/02/2025 {props.name} and age is {props.age}</h1>
-}
-export default Greetings;
--- React/my_first_app/src/add.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-const Add =(props)=>{
-    return(
-        <h1>the addition of {props.a} and {props.b} is {parseInt(props.a)+parseInt(props.b)}</h1>
-    );
-}
-export default Add;
